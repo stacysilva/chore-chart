de57743f4fa83bec273a13cbe9a0fbfb2c5011b9
Stacy Silva <stacy@deplabs.com>
2018-09-12 09:04:02 -0700

    Display User List
    Display a list of users in a Master/Detail view.
    The user can select a User and see that User's details.
    *ngFor to display a list.
    *ngIf to conditionally include or exclude a block of HTML.

---
+++ src/app/mock-users.ts
@@ -0,0 +1,9 @@
+import { User } from './user';
+
+export const USERS: User[] = [
+  { id: 11, name: 'Rachel' },
+  { id: 12, name: 'Braeden' },
+  { id: 13, name: 'Eleanor' },
+  { id: 14, name: 'Michael' },
+  { id: 15, name: 'Stacy' },
+];
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
+import { USERS } from '../mock-users';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
+
 export class UsersComponent implements OnInit {
-  user: User = {
-    id: 1,
-    name: 'Rachel'
-    };
+  users = USERS;
+
+  selectedUser: User;
 
   constructor() {}
 
   ngOnInit() {
   }
 
+  onSelect(user: User): void {
+    this.selectedUser = user;
+  }
 }
